c98cf7441f6405255eadea417769b007330baf7a
Vit DeBacco <vdebacco@gmail.com>
2023-03-25 17:10:38 -0400

    feat: initial commit

---
+++ src/main/kotlin/org/envylabs/playlistsplitter/SpotifyPlaylistSplitterApplication.kt
@@ -0,0 +1,11 @@
+package org.envylabs.playlistsplitter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyPlaylistSplitterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifyPlaylistSplitterApplication>(*args)
+}
